0d977c45c66bf6a60e08ec5098aeb46a4fa96923
Lizzy <elizabeth_zhang1@brown.edu>
2022-12-08 17:46:33 -0500

    Create RoomkastApp component

---
+++ src/routes/RoomkastApp.js
@@ -0,0 +1,13 @@
+import ProjectBanner from "../components/ProjectBanner"
+import roomkastImg from "../assets/project_thumbnails/roomkast_long.svg"
+
+const RoomkastApp = () => (
+  <>
+    <div>
+      <ProjectBanner imgSrc={roomkastImg}/>
+    </div>
+  </>
+ 
+)
+
+export default RoomkastApp
