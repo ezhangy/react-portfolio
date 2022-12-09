61bc4f37c8d20a2a2260dc88ae4cc3dac1f10349
Lizzy <elizabeth_zhang1@brown.edu>
2022-12-09 16:35:51 -0500

    Update Projects page

--- src/routes/Projects.js
+++ src/routes/Projects.js
@@ -1,48 +1,77 @@
 import ProjectItem from "../components/ProjectItem"
 import styles from "./Projects.module.css"
-import iterativeImg from "../assets/project_thumbnails/roomkast.svg"
+import iterativeImg from "../assets/project_thumbnails/roomkast.png"
 import albumImg from "../assets/project_thumbnails/album.png"
 import redesignImg from "../assets/project_thumbnails/redesign.png"
+import happinessImg from "../assets/project_thumbnails/happiness.png"
 
 const Projects = () => {
+  const categories = ["design", "development"]
   const project_data = [
     {
       id: 0,
-      title: "Responsive Redesign",
+      category: "design",
+      title: "Troop PVD's Landing Page",
       img: redesignImg,
-      description: "Description of responsive redesign",
+      description: "Redesigning a restaurant's homepage.",
       link: "site-redesign"
     },
     {
       id: 1,
-      title: "AlbumFinder",
+      category: "development",
+      title: "Album Discovery App",
       img: albumImg,
-      description: "Description of react development",
+      description: "A React app for aggregating albums.",
       link: "album-app"
     },
     {
       id: 2,
+      category: "design",
       title: "Roomkast App",
       img: iterativeImg,
-      description: "Prototyping a roommate matching app",
+      description: "Prototyping a roommate matching app.",
       link: "roomkast-app"
+    },
+    {
+      id: 2,
+      category: "development",
+      title: "Happy Places",
+      img: happinessImg,
+      description: "A visual essay about Brown students' happy places.",
+      link: "happiness-story"
     },
   ]
 
   return (
     <div className="page">
-      <h1>My Projects</h1>
+      <div className={styles["intro-text-container"]}>
+        <h1 id={styles["hello-text"]}>hi there, i'm <b>lizzy.</b></h1>
+        <p>I'm a <b>developer</b> and <b>designer</b> passionate about creating joyful tools and experiences.</p>
+        <div id={styles["see-work-wrapper"]}>
+          <h2>works ↓</h2>
+        </div>
+      </div>
+      
       <div className={styles.projects}>
-      {project_data.map((project) => 
-        <ProjectItem
-          key={project.id}
-          title={project.title}
-          thumbnail={project.img}
-          descr={project.description}
-          link={project.link}
-         />
-      )}
-    </div>
+        {categories.map((category) => (
+          <div className="projectCategory" key={category}>
+            <h2 className={styles["category-title"]}>{category}</h2>
+            <div className={styles["projects-list"]}>
+              {project_data.filter((project) => {
+                  return category === project.category
+              }).map((project) => 
+                <ProjectItem
+                  key={project.id}
+                  title={project.title}
+                  thumbnail={project.img}
+                  descr={project.description}
+                  link={project.link}
+                />
+              )}
+            </div>
+          </div>
+        ))}
+      </div>
     </div>
     
   )
